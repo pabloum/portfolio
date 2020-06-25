7e8e4a8833769fda29c540d64be960603301bfd5
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-06-25 17:37:09 -0500

    Add Partial views with personal information

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Portfolio.Core;
+using Portfolio.Data;
 
 namespace Portfolio.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IPablosData pablosData;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public PersonalInformation Pablo { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IPablosData pablosData)
         {
             _logger = logger;
+            this.pablosData = pablosData;
         }
 
         public void OnGet()
         {
-
+            Pablo = pablosData.GetInformation();
         }
     }
 }
