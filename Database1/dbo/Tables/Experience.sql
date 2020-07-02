a73ae598f267722434cb50ee18c66bda2724223b
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-07-02 11:11:35 -0500

    Update column type on Experience table

--- Database1/dbo/Tables/Experience.sql
+++ Database1/dbo/Tables/Experience.sql
@@ -1,12 +1,14 @@
 ﻿CREATE TABLE [dbo].[Experience] (
-    [Id]              INT           NOT NULL,
-    [Company]         VARCHAR (50)  NULL,
-    [Position]        VARCHAR (50)  NULL,
-    [MainFunctions]   VARCHAR (MAX) NULL,
-    [Technologies]    VARCHAR (256) NULL,
-    [DateBegining]    DATETIME      NULL,
-    [DateEnd]         DATETIME      NULL,
-    [YearsExperience] NUMERIC (18)  NULL,
+    [Id]              INT             NOT NULL,
+    [Company]         VARCHAR (50)    NULL,
+    [Position]        VARCHAR (50)    NULL,
+    [MainFunctions]   VARCHAR (MAX)   NULL,
+    [Technologies]    VARCHAR (256)   NULL,
+    [DateBegining]    DATETIME        NULL,
+    [DateEnd]         DATETIME        NULL,
+    [YearsExperience] NUMERIC (18, 2) NULL,
     CONSTRAINT [PK_Experience] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
