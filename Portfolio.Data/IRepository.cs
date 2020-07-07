ff60b166e6eda6af019b4ee5d3ff3d7d06534924
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-07-07 12:48:34 -0500

    Add documentation to IRepository interface

--- Portfolio.Data/IRepository.cs
+++ Portfolio.Data/IRepository.cs
@@ -4,13 +4,47 @@ using System.Text;
 
 namespace Portfolio.Data
 {
+    /// <summary>
+    /// Interface for all Entities DAOs (Data Access Objects)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IRepository<T>
     {
+        /// <summary>
+        /// Retrieves all records of table
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// Creates an entity
+        /// </summary>
+        /// <param name="entity"></param>
         void Create(T entity);
+
+        /// <summary>
+        /// Gets an record by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         T Read(int id);
+
+        /// <summary>
+        /// Updates an entity
+        /// </summary>
+        /// <param name="entity"></param>
         void Update(T entity);
+
+        /// <summary>
+        /// Deletes an entity
+        /// </summary>
+        /// <param name="entity"></param>
         void Delete(T entity);
+
+        /// <summary>
+        /// Save changes in the DataBase
+        /// </summary>
+        /// <returns></returns>
         int Commit();
     }
 }
