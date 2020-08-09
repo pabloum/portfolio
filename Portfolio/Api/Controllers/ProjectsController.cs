a14596524d4edeb31d7ac0502cfe8efda063fb4f
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-08-09 10:56:39 -0500

    Add the rest of the Api Controllers

---
+++ Portfolio/Api/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Api.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Api.Controllers
+{
+    public class ProjectsController : PortfolioBaseController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("We are working on this Endpoint");
+        }
+    }
+}
