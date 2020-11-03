44238b857b5b02b1ab1fdfd75a14590c45b672d2
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-11-03 18:08:54 -0500

    Add Simple Factory for skills

---
+++ Portfolio.Data/SkillFactory/SkillFactory.cs
@@ -0,0 +1,15 @@
+﻿using Portfolio.Entities;
+using Portfolio.Entities.Entities;
+
+namespace Portfolio.Data
+{
+    public static class SkillFactory
+    {
+        private static int IdCounter = 0;
+
+        public static Skill CreateSkill(string name, int percentage, SkillType type)
+        {
+            return new Skill { Id = ++IdCounter, Name = name, Percentage = percentage, Category = type };
+        }
+    }
+}
